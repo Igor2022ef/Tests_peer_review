4b89ba81a3eecf256120fce607f813c4f25b4b71
Igor2022ef <2023ief@gmail.com>
2023-03-22 18:16:56 +0300

    Задание выполнено, запускается только под VPN

--- test_items.py
+++ test_items.py
@@ -1,11 +1,17 @@
 import time
 from selenium.webdriver.common.by import By
 
+'''
+Комментарий для проверяющего:
+Код написан для PyCharm строки обращения к driver возможно нужно будет заменить, 
+как и к user_language
+'''
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_should_check_basket(browser):
-
     browser.get(link)
+    time.sleep(30)
     input1 = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     text_basket = input1.text
     print(f"'Это текст на кнопке:' {text_basket}")
